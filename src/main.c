95e246f235fecdbc0ab894e6b0ddf26b64856ae0
daniel <danielbnhz@gmail.com>
2025-12-21 03:44:55 -0800

    adds programming to serve minimal HTTP response over TCP

--- src/main.c
+++ src/main.c
@@ -4,66 +4,71 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 4096
 
 int main(void) {
     int server_fd, client_fd;
     struct sockaddr_in addr;
     socklen_t addr_len = sizeof(addr);
+    char buffer[BUFFER_SIZE];
 
-    // 1. Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
         perror("socket");
         return 1;
     }
 
-    // 2. Allow quick restart (important on Linux)
     int opt = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // 3. Bind socket to IP + port
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = INADDR_ANY; // 0.0.0.0
+    addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("bind");
-        close(server_fd);
         return 1;
     }
 
-    // 4. Start listening
-    if (listen(server_fd, 1) < 0) {
+    if (listen(server_fd, 10) < 0) {
         perror("listen");
-        close(server_fd);
         return 1;
     }
 
-    printf("Server listening on port %d...\n", PORT);
+    printf("HTTP server listening on port %d...\n", PORT);
 
-    // 5. Accept a connection
     client_fd = accept(server_fd, (struct sockaddr *)&addr, &addr_len);
     if (client_fd < 0) {
         perror("accept");
-        close(server_fd);
         return 1;
     }
 
-    printf("Client connected!\n");
+    // Read HTTP request
+    int bytes = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes > 0) {
+        buffer[bytes] = '\0';
+        printf("Request:\n%s\n", buffer);
+    }
 
-    // 6. Send data
-    const char *msg = "Hello from C TCP server!\n";
-    send(client_fd, msg, strlen(msg), 0);
+    // HTTP response body
+    const char *body = "Hello from your C HTTP server!\n";
 
-    // 7. Cleanup
+    // Build HTTP response
+    char response[BUFFER_SIZE];
+    snprintf(response, sizeof(response),
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Length: %zu\r\n"
+        "Content-Type: text/plain\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "%s",
+        strlen(body), body
+    );
+
+    send(client_fd, response, strlen(response), 0);
+
     close(client_fd);
     close(server_fd);
 
