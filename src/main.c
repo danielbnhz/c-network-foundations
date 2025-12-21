8e323116a856e10cc9f7f93fef0dd005ee7227ef
daniel <danielbnhz@gmail.com>
2025-12-21 03:38:40 -0800

    updates gitignore and makes a basic server skeleton

--- src/main.c
+++ src/main.c
@@ -1,6 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 8080
 
 int main(void) {
-    printf("C network foundations initialized.\n");
+    int server_fd, client_fd;
+    struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(addr);
+
+    // 1. Create socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    // 2. Allow quick restart (important on Linux)
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    // 3. Bind socket to IP + port
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY; // 0.0.0.0
+
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("bind");
+        close(server_fd);
+        return 1;
+    }
+
+    // 4. Start listening
+    if (listen(server_fd, 1) < 0) {
+        perror("listen");
+        close(server_fd);
+        return 1;
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    // 5. Accept a connection
+    client_fd = accept(server_fd, (struct sockaddr *)&addr, &addr_len);
+    if (client_fd < 0) {
+        perror("accept");
+        close(server_fd);
+        return 1;
+    }
+
+    printf("Client connected!\n");
+
+    // 6. Send data
+    const char *msg = "Hello from C TCP server!\n";
+    send(client_fd, msg, strlen(msg), 0);
+
+    // 7. Cleanup
+    close(client_fd);
+    close(server_fd);
+
     return 0;
 }
